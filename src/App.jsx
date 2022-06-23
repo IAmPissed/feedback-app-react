953047e0ddb369cb2bedd936dfbf5f980e0a29e7
Xaniar Vakili <joker427861@gmail.com>
2022-06-23 09:51:17 +0430

    place Header outside the container

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Header from "./components/Header"
 function App() {
 	return (
 		<>
-			<div className='container'>
-				<Header />
-			</div>
+			<Header />
+			<div className='container'></div>
 		</>
 	)
 }
