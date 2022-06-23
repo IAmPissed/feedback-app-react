35ba26b92d84494c093cbd996128c3a72cdc0cfa
Xaniar Vakili <joker427861@gmail.com>
2022-06-23 10:13:05 +0430

    add placeholder feedback data to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
+import FeedbackData from "./data/feedbackData"
 import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
 
 function App() {
+	const [feedback, setFeedback] = useState(FeedbackData)
+
 	return (
 		<>
 			<Header />
-			<div className='container'></div>
+			<div className='container'>
+				<FeedbackItem />
+			</div>
 		</>
 	)
 }
