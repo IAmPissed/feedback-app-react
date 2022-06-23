8e91c9e3361bead569e626c8dee71cc35f67b76c
Xaniar Vakili <joker427861@gmail.com>
2022-06-23 09:37:05 +0430

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header"
+
 function App() {
 	return (
 		<>
-			<div>Hello from the app component</div>
+			<div className='container'>
+				<Header />
+			</div>
 		</>
 	)
 }
