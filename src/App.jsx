c4b85016ace64805614728a29a02bee84a2666ab
Xaniar Vakili <joker427861@gmail.com>
2022-06-24 14:41:53 +0430

    add about page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { v4 as uuidv4 } from "uuid"
 import FeedbackData from "./data/feedbackData"
 import Header from "./components/Header"
 import FeedbackForm from "./components/FeedbackForm"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackList from "./components/FeedbackList"
+import AboutIconLink from "./components/AboutIconLink"
+import About from "./pages/About"
 
 function App() {
 	const [feedbacks, setFeedbacks] = useState(FeedbackData)
@@ -22,14 +25,28 @@ function App() {
 	}
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className='container'>
-				<FeedbackForm handleAddFeedback={addFeedback} />
-				<FeedbackStats feedbacks={feedbacks} />
-				<FeedbackList feedbacks={feedbacks} handleDelete={handleDelete} />
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<>
+								<FeedbackForm handleAddFeedback={addFeedback} />
+								<FeedbackStats feedbacks={feedbacks} />
+								<FeedbackList
+									feedbacks={feedbacks}
+									handleDelete={handleDelete}
+								/>
+							</>
+						}
+					></Route>
+					<Route path='/about' element={<About />}></Route>
+				</Routes>
+				<AboutIconLink />
 			</div>
-		</>
+		</Router>
 	)
 }
 export default App
